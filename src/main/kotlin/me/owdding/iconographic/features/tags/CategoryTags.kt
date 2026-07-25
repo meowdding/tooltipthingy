50a34ae936ff20594074036319da0512f4b4ee44
Rain <178751120+Rainymei@users.noreply.github.com>
2026-07-25 16:42:03 +0200

    Feature(Enchanted Books): Adds tag and name+tooltip edit (#44)

--- src/main/kotlin/me/owdding/iconographic/features/tags/CategoryTags.kt
+++ src/main/kotlin/me/owdding/iconographic/features/tags/CategoryTags.kt
@@ -20,6 +20,10 @@ data object CategoryTags : TooltipFeature() {
     private val idRegex = Regex("(?i)^(?<name>.+?)\\s*\\(\\s*ID\\s+(?<id>[A-Z0-9]+)\\s*\\)$")
 
     override fun ItemStack.leftTags(): List<TooltipTag> {
+        if (DataTypes.SKYBLOCK_ID()?.isEnchantment == true) {
+            return listOf(TooltipTag.literal("ENCHANTED BOOK", 0xFF66FF))
+        }
+
         val category = DataTypes.CATEGORY() ?: return emptyList()
 
         if (!category.isDungeon && category.name.isEmpty()) return emptyList()
