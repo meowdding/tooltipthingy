bf87b3d915334c87ebaa5a3c30467b7081c0e949
JustAlex <justalex7473@gmail.com>
2026-08-04 04:54:00 -0400

    refactor: make it use the lineModifier stuff

--- src/main/kotlin/me/owdding/iconographic/features/tags/PetTags.kt
+++ src/main/kotlin/me/owdding/iconographic/features/tags/PetTags.kt
@@ -1,20 +1,13 @@
 package me.owdding.iconographic.features.tags
 
-import me.owdding.iconographic.ExtractableTooltipLine
 import me.owdding.iconographic.TooltipLine
 import me.owdding.iconographic.config.categories.tag.TagConfig
-import me.owdding.iconographic.font
 import me.owdding.iconographic.system.RegisterFeature
 import me.owdding.iconographic.system.Result
 import me.owdding.iconographic.system.TooltipFeatureWithContext
 import me.owdding.iconographic.system.TooltipTag
-import net.minecraft.client.gui.Font
-import net.minecraft.client.gui.GuiGraphicsExtractor
 import net.minecraft.network.chat.Component
 import net.minecraft.world.item.ItemStack
-import tech.thatgravyboat.skyblockapi.utils.regex.component.match
-import tech.thatgravyboat.skyblockapi.utils.regex.component.toComponentRegex
-import tech.thatgravyboat.skyblockapi.utils.text.Text.prefix
 import tech.thatgravyboat.skyblockapi.utils.text.TextProperties.stripped
 import tech.thatgravyboat.skyblockapi.utils.text.TextUtils.substring
 
@@ -24,30 +17,45 @@ data object PetTags : TooltipFeatureWithContext<MutableList<String>>() {
     override fun createContext(): MutableList<String> = mutableListOf()
     override val priority: Int = 2
 
-    private val skinRegex = Regex(".* (?:Pet|Mount|Morph),\\s(?<skinName>.* Skin)").toComponentRegex()
-
     val petCategories = buildMap {
         fun pet(skill: String) {
-            put("$skill Pet", PetCategory(skill, "Pet"))
+            put(
+                "$skill Pet",
+                PetCategory(skill, "Pet") {
+                    it.substring("$skill Pet, ".length)
+                },
+            )
         }
 
         fun mount(skill: String) {
-            put("$skill Mount", PetCategory(skill, "Mount"))
+            put(
+                "$skill Mount",
+                PetCategory(skill, "Mount") {
+                    it.substring("$skill Mount, ".length)
+                },
+            )
         }
         put("All Skills", PetCategory("Pet"))
         pet("Mining")
         mount("Mining")
         pet("Combat")
         mount("Combat")
-        put("Combat Morph", PetCategory("Combat", "Morph"))
+        put(
+            "Combat Morph",
+            PetCategory("Combat", "Morph") {
+                it.substring("Combat Morph, ".length)
+            },
+        )
         pet("Foraging")
         pet("Enchanting")
         pet("Taming")
         pet("Farming")
-        put("Gabagool Pet, feed to gain XP", PetCategory("Gabagool", "Pet") {
-            val line = it.substring("Gabagool Pet, ".length + 1)
-            line.prefix("F").withStyle(line.style)
-        })
+        put(
+            "Gabagool Pet, feed to gain XP",
+            PetCategory("Gabagool", "Pet") {
+                it.substring("Gabagool Pet, ".length)
+            },
+        )
         pet("Alchemy")
         pet("Fishing")
         pet("Fractured Soul")
@@ -67,14 +75,13 @@ data object PetTags : TooltipFeatureWithContext<MutableList<String>>() {
         if (!canRead()) return@withComponentMerger Result.unmodified
         val line = read()
         val category = petCategories[line.stripped.removeSkin().trim().lowercase()] ?: return@withComponentMerger Result.unmodified
-        if (category.lineModifier != null) {
-            add(category.lineModifier(line))
+        if ((category.lineModifier != null)) {
+            val modifiedLine = category.lineModifier(line)
+            if (modifiedLine != Component.empty()) {
+                add(modifiedLine)
+            }
         } else {
             skipSpace()
-        }
-
-        skinRegex.match(line, "skinName") { [skinName] ->
-            originalMerger.destination.add(1,skinLine(skinName))
         }
 
         context.addAll(category.tags)
@@ -83,23 +90,8 @@ data object PetTags : TooltipFeatureWithContext<MutableList<String>>() {
 
     data class PetCategory(
         val tags: List<String>,
-        val lineModifier: ((Component) -> Component)? = null
+        val lineModifier: ((Component) -> Component)? = null,
     ) {
         constructor(vararg tags: String, lineModifier: ((Component) -> Component)? = null) : this(tags.asList(), lineModifier)
-    }
-
-    data class skinLine(
-        val skinComponent: Component,
-    ) : ExtractableTooltipLine {
-
-        override fun extract(graphics: GuiGraphicsExtractor, totalWidth: Int, x: Int, y: Int) {
-            graphics.text(font, skinComponent, x, y, -1)
-        }
-
-        override fun getWidth(font: Font): Int {
-            return font.width(skinComponent)
-        }
-
-        override fun getHeight(font: Font): Int = font.lineHeight
     }
 }
