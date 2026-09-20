6556e68f5c14a6a455cdb8fc73829a059a121916
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-09-20 17:38:13 +0200

    refactor: remove useless code in RarityTag

--- src/main/kotlin/me/owdding/iconographic/features/tags/RarityTag.kt
+++ src/main/kotlin/me/owdding/iconographic/features/tags/RarityTag.kt
@@ -1,24 +1,19 @@
 package me.owdding.iconographic.features.tags
 
-import com.mojang.brigadier.arguments.FloatArgumentType
 import me.owdding.iconographic.config.categories.tag.TagConfig
 import me.owdding.iconographic.system.RegisterFeature
 import me.owdding.iconographic.system.TooltipFeature
 import me.owdding.iconographic.system.TooltipTag
 import me.owdding.iconographic.utils.chat.ChatUtils
 import me.owdding.iconographic.utils.chat.DisplayColor.displayColor
-import me.owdding.iconographic.utils.chat.sendWithPrefix
-import me.owdding.iconographic.utils.debug.RegisterIconCommandEvent
 import me.owdding.ktmodules.Module
 import me.owdding.lib.rendering.text.builtin.GradientTextShader
 import me.owdding.lib.rendering.text.textShader
+import net.minecraft.util.Util
 import net.minecraft.world.item.ItemStack
 import tech.thatgravyboat.skyblockapi.api.data.SkyBlockRarity
 import tech.thatgravyboat.skyblockapi.api.datatype.DataTypes
-import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
-import tech.thatgravyboat.skyblockapi.api.events.misc.RegisterCommandsEvent.Companion.argument
 import tech.thatgravyboat.skyblockapi.api.remote.api.SkyBlockId
-import tech.thatgravyboat.skyblockapi.utils.command.EnumArgument
 import tech.thatgravyboat.skyblockapi.utils.text.Text
 import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.font
 
@@ -29,7 +24,7 @@ data object RarityTag : TooltipFeature() {
 
     // TODO: probably repofy this?
     private val disallowedIds = listOf(
-        SkyBlockId.item("skyblock_menu")
+        SkyBlockId.item("skyblock_menu"),
     )
 
     override fun ItemStack.applies(): Boolean = DataTypes.SKYBLOCK_ID() != null && DataTypes.SKYBLOCK_ID() !in disallowedIds
@@ -60,62 +55,29 @@ data object RarityTag : TooltipFeature() {
 
 @Module
 object ShaderCache {
-    private val cache: MutableMap<SkyBlockRarity, GradientTextShader> = mutableMapOf()
 
-    private var shaderSpeed = 3f
-        set(value) {
-            field = value
-            cache.clear()
-        }
-    private var shaderDir = GradientTextShader.Direction.UP
-        set(value) {
-            field = value
-            cache.clear()
-        }
-
-    fun getOrRegisterUpgradeShader(rarity: SkyBlockRarity): GradientTextShader {
-        return cache.getOrPut(rarity) {
-            val previous = rarity.getPreviousRarity()
-            val startColor = previous.displayColor
-            val endColor = rarity.displayColor
-            GradientTextShader(
-                colors = listOf(
-                    endColor,
-                    startColor,
-                    endColor,
-                    endColor,
-                    endColor,
-                    endColor,
-                ),
-                direction = shaderDir,
-                speed = shaderSpeed,
-            )
-        }
+    private val cache = Util.memoize { rarity: SkyBlockRarity ->
+        val previous = rarity.getPreviousRarity()
+        val startColor = previous.displayColor
+        val endColor = rarity.displayColor
+        GradientTextShader(
+            colors = listOf(
+                endColor,
+                startColor,
+                endColor,
+                endColor,
+                endColor,
+                endColor,
+            ),
+            direction = GradientTextShader.Direction.UP,
+            speed = 3f,
+        )
     }
+
+    fun getOrRegisterUpgradeShader(rarity: SkyBlockRarity): GradientTextShader = cache.apply(rarity)
 
     private fun SkyBlockRarity.getPreviousRarity(): SkyBlockRarity {
         if (ordinal <= 0) return this
         return SkyBlockRarity.entries[ordinal - 1]
-    }
-
-    // TODO: Remove after figuring out the values
-    @Subscription
-    fun onRegisterCommand(event: RegisterIconCommandEvent) {
-        event.registerDev("shader") {
-            then("speed", FloatArgumentType.floatArg(0f)) {
-                callback {
-                    val newSpeed = argument<Float>("speed")
-                    shaderSpeed = newSpeed
-                    Text.of("Set shader speed to $newSpeed").sendWithPrefix()
-                }
-            }
-            then("direction", EnumArgument.create(GradientTextShader.Direction::class.java)) {
-                callback {
-                    val newDir = argument<GradientTextShader.Direction>("direction")
-                    shaderDir = newDir
-                    Text.of("Set shader direction to $newDir").sendWithPrefix()
-                }
-            }
-        }
     }
 }
